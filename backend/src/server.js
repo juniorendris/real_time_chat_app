18f2a67319753ba8ab9c6d01844d1f80e2a01695
juniorendris <juniorend292@gmail.com>
2026-09-08 02:55:27 +0300

    prepare app for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cookie_parser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
-
+const path = require("path");
 app.use(
   cors({
     origin:process.env.CLIENT_URL,
@@ -14,11 +14,21 @@ app.use(
 app.use(express.json());
 app.use(cookie_parser());
 const PORT = process.env.PORT || 3000;
+const __direname=path.resolve();//get curent path
 const { createTable } = require("./config");
 
 app.use("/api/auth/", require("./routes/auth.Routes"));
 app.use("/api/user/", require("./routes/user.Routes"));
 app.use("/api/chat/", require("./routes/chat.Routes"));
+
+// production deployment 
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__direname,"../frontend/dist")));
+  app.use((req,res)=>{
+    res.sendFile(path.join(__direname,"../frontend/dist/index.html"));
+  });}
+
+
 app.listen(PORT, async () => {
   try {
     console.log(`server is running on port ${PORT} `);
