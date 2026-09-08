ba6e13e6f9baa30df5e6bf82313a720882c123aa
juniorendris <juniorend292@gmail.com>
2026-09-08 04:00:05 +0300

    update database configuration

--- backend/src/config/pool.js
+++ backend/src/config/pool.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2/promise");
 const config = {
   user: process.env.USER,
-  port: process.env.PORT,
+ port: process.env.PORT_DB,
   host: process.env.HOST,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
